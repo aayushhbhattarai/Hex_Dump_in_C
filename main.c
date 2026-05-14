a3114c19eea7a9cb449da2b0495e7c41a7afa529
aayushhbhattarai <baayush061@gmail.com>
2026-05-14 17:13:09 +0200

    3

--- main.c
+++ main.c
@@ -11,13 +11,32 @@ int main()
     {
         int ch;
         int count = 0;
+        int add = 0;
+        unsigned char tray[16];
+        printf("%08X: ", add);
         while ((ch = fgetc(ptr)) != EOF)
         {
             printf("%02X ", ch);
+            tray[count] = ch;
             count++;
+
             if (count == 16)
             {
+                printf(" | ");
+                for (int i = 0; i < 16; i++)
+                {
+                    if (tray[i] >= 32 && tray[i] <= 126)
+                    {
+                        printf("%c", tray[i]);
+                    }
+                    else
+                    {
+                        printf(" . ");
+                    }
+                }
                 printf("\n");
+                add += 16;
+                printf("%08X: ", add);
                 count = 0;
             }
         }
